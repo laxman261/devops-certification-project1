f97bc5b35e007bcbfd67f5f455c19d271a98e1d7
prashantsuragimath <109673973+prashantsuragimath@users.noreply.github.com>
2023-01-18 21:42:20 +0530

    Update TestCalculator.java

--- src/test/java/staragile/demo/utilities/TestCalculator.java
+++ src/test/java/staragile/demo/utilities/TestCalculator.java
@@ -8,46 +8,43 @@ import org.slf4j.LoggerFactory;
 
 import com.staragile.demo.utilities.LoggerStackTraceUtil;
 
-public class TestCalculator {
+public class AddressbookUI extends UI {
 	
 
-    private static final Logger LOG = LoggerFactory.getLogger(TestCalculator.class);
-    @Test
-    public void testSum() {
-          int num1=10;
-          int num2=20;
-          int sum=num1+num2;
-          assertEquals("sum is:", 30, sum);
-          
-            
+    public static void main( String[] args ) throws InterruptedException
+    {
+        //System.out.println( "Hello World!" );
+	
+		System.setProperty("webdriver.chrome.driver","/root/pkcs/chromedriver");
+		//System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
+		
+		
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		//chromeOptions.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		
+		System.out.println("Welcome to the Selenium Scripts - It starts executing here");
+		//automation
+		driver.get("http://54.197.91.205:8080/addressbook/");
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		driver.findElement(By.className("v-button")).click();
+		driver.findElement(By.id("gwt-uid-5")).sendKeys("prashant");
+		driver.findElement(By.id("gwt-uid-7")).sendKeys("Suragimath");
+		driver.findElement(By.id("gwt-uid-9")).sendKeys("9999999999");
+		driver.findElement(By.id("gwt-uid-11")).sendKeys("pkcsmath@gmail.com");
+		driver.findElement(By.id("gwt-uid-13")).sendKeys("18/01/2023");
+		Thread.sleep(2000);
+		//click on the save button
+		driver.findElement(By.className("v-button-primary")).click();
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
+		System.out.println("Script executed successfully");
+		
     }
-    
-    @Test
-    public void testSubstract() {
-          int num1=20;
-          int num2=10;
-          int difference=num1-num2;
-          assertEquals("difference is :", 10, difference);
-          
-            
-    }
-    
-    @Test
-    public void testMultiply() {
-          int num1=20;
-          int num2=30;
-          int multiply=num1*num2;
-          assertEquals("multiply is :", 600, multiply);
-            
-    }
-    
-    @Test
-    public void testDivide() {
-    	int num1 = 100;
-    	int num2 = 5;
-    	int divide = num1/num2;
-    	assertEquals("division is :",20,divide);
-    }
-
-
 }
