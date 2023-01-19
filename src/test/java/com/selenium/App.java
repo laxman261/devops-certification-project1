95291ebd4ac590c4330f9093990bba21d09baaaa
prashantsuragimath <109673973+prashantsuragimath@users.noreply.github.com>
2023-01-19 19:12:06 +0530

    Update App.java

--- src/test/java/com/selenium/App.java
+++ src/test/java/com/selenium/App.java
@@ -1,15 +1,22 @@
 package com.selenium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/root/pkcs/chromedriver");
-		WebDriver driver=new ChromeDriver();
-		driver.navigate().to("http://54.197.91.205:8080/addressbook/");
-		driver.findElement(By.name("loginbutton")).click();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://18.212.179.102:8080/addressbook/");
 	}
 }
+			
+			
+			
+			
+			
+			
+			
+			
 		
